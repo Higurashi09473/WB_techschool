6d9d938c44dafdd95729a1f437856bd2c79b4be6
higu <ilya-isakov-2018@bk.ru>
2025-08-28 12:22:12 +0300

    add L1.1

---
+++ L1/L1.1/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	Name string
+	Age int
+}
+
+func (h *Human) Speak() {
+    fmt.Printf("%s говорит: Привет, мне %d лет!\n", h.Name, h.Age)
+}
+
+func (h *Human) Walk() {
+    fmt.Println(h.Name, "идёт пешком.")
+}
+
+func (a *Action) Run() {
+	a.Energy--
+    fmt.Println(a.Name, "бежит")
+	fmt.Println(a.Energy)
+}
+
+// Дочерняя структура Action, встраивающая Human (embedding)
+type Action struct {
+    Human      // Встраивание: нет имени поля — это ключ!
+    Energy int // Своё поле: энергия
+}
+
+func main() {
+	higu := &Action{
+		Human: Human{
+			Name: "Higu",
+			Age: 19,
+		},
+		Energy: 20,
+	}
+	higu.Walk()
+	higu.Run()
+}
+
+
+
+/*
+Дана структура Human (с произвольным набором полей и методов).
+Реализовать встраивание методов в структуре Action от родительской структуры Human (аналог наследования).
+
+Подсказка: используйте композицию (embedded struct), чтобы Action имел все методы Human.
+*/
+
+
+/* композиция (composition) / встраивание структур (embedding) */
