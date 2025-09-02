c2048e7cc85c3d38da98281c86d488ed48d3b379
higu <ilya-isakov-2018@bk.ru>
2025-09-02 14:43:04 +0300

    add L1.7

---
+++ L1/L1.7/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	set := make(map[int]string)
+	mt := sync.Mutex{}
+	wg := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			mt.Lock()
+			set[i] = fmt.Sprint("data ", i)
+			mt.Unlock()
+		}()
+	}
+
+	wg.Wait()
+	for j := 0; j < 100; j++ {
+		fmt.Println(set[j])
+	}
+}
