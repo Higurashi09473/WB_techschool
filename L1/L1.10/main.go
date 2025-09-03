cf778cee6f7cffd5a52a543e58bfdee675512142
Higu <ilya-isakov-2018@bk.ru>
2025-09-03 09:40:41 +0300

    add L1.L10

---
+++ L1/L1.10/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	array := []float64{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	set := make(map[int][]float64)
+
+	for _, value := range array {
+		key := int(value) - int(value)%10
+		set[key] = append(set[key], value)
+	}
+
+	for i, v := range set {
+		fmt.Println(i, v)
+	}
+}
