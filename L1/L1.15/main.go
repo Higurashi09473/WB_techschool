cfdff6125bc454f2e0645434162e98df9eb216b9
higu <ilya-isakov-2018@bk.ru>
2025-09-04 08:23:17 +0300

    update L1.15

--- L1/L1.15/main.go
+++ L1/L1.15/main.go
@@ -1,15 +1,18 @@
 package main
 
-func createHugeString(i int) []byte {
-	return make([]byte, i)
+
+var justString string
+
+func createHugeString(i int) string {
+	return string(make([]byte, i))
 }
 
-func someFunc() string{
+func someFunc(){
 	// При создании большой строки justString она имеет указатель на тот же массив что и v, это не дает сборщику мусора удалить v (утечка памяти)
 	v := createHugeString(1 << 10)
 
 	//Мы возвращаем данные и после GC может удалить v
-	return string(v[:100])
+	justString = string([]byte(v[:100]))
 }
 
 
