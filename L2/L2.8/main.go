43621ed76a16c6c66cc81b143e5d0e1877206ffe
higu <higurashi09473@gmail.com>
2026-02-26 03:53:17 +0000

    add L2.8

--- L2/L2.8/main.go
+++ L2/L2.8/main.go
@@ -1,1 +1,19 @@
-package l28
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+func main() {
+	t, err := ntp.Time("0.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "NTP error: ", err)
+		os.Exit(1)
+	}	
+
+	fmt.Println(t.Format(time.RFC3339Nano))
+}
